612537ef1604384ba4cfac3edf623d4d6810b330
Enes Polat <37534266+enespolat25@users.noreply.github.com>
2021-09-17 00:29:01 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Translate from './Translate';
+
+function App() {
+  return (
+    <div className="App">
+    <Translate/>
+     
+    </div>
+  );
+}
+
+export default App;
